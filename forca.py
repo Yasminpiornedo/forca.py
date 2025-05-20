267a717eed7fd09ffa23a750fe4519ad9fa6d4eb
Yasminpiornedo <159542694+Yasminpiornedo@users.noreply.github.com>
2025-05-20 09:02:51 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,54 @@
+import random 
+
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    arquivo = open("jogos/palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+
+    arquivo.close()
+    
+    aleatoria = random.randrange(0, len(palavras))
+    palavra_secreta  = palavras[aleatoria].upper()
+
+    palavras = []
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    enforcou = False
+    acertou = False
+    erros = 0 
+
+    while (not enforcou and not acertou):
+       
+        chute = input("Qual é a letra?")
+        chute = chute.strip().upper()
+ 
+        if(chute in palavra_secreta):
+            index = 0
+            for letras in  palavra_secreta:
+                if(chute == letras):
+                    letras_acertadas[index] = letras
+                index = index + 1   
+        else:
+            erros = erros + 1
+
+        enforcou = erros == 8
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+        if(acertou):
+            print("Você ganhou!!")
+        else:
+            print("Você perdeu, seu bosta!")    
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
